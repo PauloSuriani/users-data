57908fd50b9bfa0ffd7b684e1cc14e28716df4c7
PauloSuriani <paulosuriani@gmail.com>
2022-10-03 16:32:34 -0300

    feat: add user controller and error handling

---
+++ users-data-backend/src/controllers/user.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import BadRequest from '../errors/badRequest';
+import NotFound from '../errors/notFound';
+import UserInterface from '../interfaces/user';
+import { UserService } from '../services/user';
+
+export async function create(req: Request, res: Response, next: NextFunction) {
+  const { name } = req.body as UserInterface;
+  const userService = new UserService();
+  try {
+    if (name === undefined) {
+      throw new BadRequest('Você precisa enviar o nome da pessoa');
+    }
+    await userService.create({ name });
+    res.status(201).send();
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function find(req: Request, res: Response, next: NextFunction) {
+  const { id } = req.params;
+  const userService = new UserService();
+  try {
+    if (id === undefined) {
+      throw new BadRequest('Você precisa enviar o id da pesquisa');
+    }
+    const obj = await userService.find(parseInt(id, 10));
+    if (!obj) {
+      throw new NotFound('Pessoa não encontrada');
+    }
+    res.status(200).json(obj);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function list(_req: Request, res: Response, next: NextFunction) {
+  const userService = new UserService();
+  try {
+    const userList = await userService.list();
+    res.json(userList);
+  } catch (err) {
+    next(err);
+  }
+}
