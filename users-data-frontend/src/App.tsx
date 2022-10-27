881fc376d8c13b34495ba577752bcd30939d90c4
PauloSuriani <paulosuriani@gmail.com>
2022-10-26 22:46:40 -0300

    feat: add react router dom

--- users-data-frontend/src/App.tsx
+++ users-data-frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { MainPage } from "./pages/MainPage";
+import { AppRouter } from "./routes/AppRouter";
 
 function App() {
 
   return (
     <div className="App">
-      <MainPage />
+      <AppRouter />
     </div>
   )
 }
---
+++ users-data-frontend/src/routes/AppRouter.tsx
@@ -0,0 +1,27 @@
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Routes,
+    RouteObject
+  } from 'react-router-dom';
+import { EditCustommer } from '../pages/EditCustommer';
+import { MainPage } from "../pages/MainPage";
+import { NewCustommerForm } from '../pages/NewCustommerForm';
+
+type TParams = { id: string };
+
+// function Product({ match }: RouteObject<TParams>) {
+//   return <h2>This is a page for product with ID: {match.params.id} </h2>;
+// }
+
+export function AppRouter() {
+return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<MainPage/>} />
+      <Route path="/newcustommer" element={<NewCustommerForm/>} />
+      <Route path="/custommer/edit/:id" element={<EditCustommer/>} />
+    </Routes>
+  </Router>
+);} 
