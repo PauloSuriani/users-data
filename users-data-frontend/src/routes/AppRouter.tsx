b5169ae5fc8e500b5a6a23d85d18e3cdffe5e502
PauloSuriani <paulosuriani@gmail.com>
2023-02-20 21:33:36 -0300

    feat: add product routes

--- users-data-frontend/src/routes/AppRouter.tsx
+++ users-data-frontend/src/routes/AppRouter.tsx
@@ -6,7 +6,10 @@ import {
 import { EditCustommer } from '../pages/EditCustommer';
 import { MainPage } from "../pages/MainPage";
 import { LoginPage } from "../pages/LoginPage"
+import { ProductsPage } from "../pages/ProductsPage"
 import { NewCustommerForm } from '../pages/NewCustommerForm';
+import { NewProduct } from '../pages/NewProductForm';
+import { EditProduct } from '../pages/EditProduct';
 
 export function AppRouter() {
 return (
@@ -14,8 +17,11 @@ return (
     <Routes>
       <Route path="/" element={<MainPage/>} />
       <Route path="/login" element={<LoginPage/>} />
-      <Route path="/newcustommer" element={<NewCustommerForm/>} />
+      <Route path="/products" element={<ProductsPage/>} />
+      <Route path="/product/edit/:id" element={<EditProduct/>} />
+      <Route path="/newproduct" element={<NewProduct/>} />
       <Route path="/custommer/edit/:id" element={<EditCustommer/>} />
+      <Route path="/newcustommer" element={<NewCustommerForm/>} />
     </Routes>
   </Router>
 );} 
