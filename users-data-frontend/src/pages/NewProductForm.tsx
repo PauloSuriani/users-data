ec1062262aa15bf64200bde3dcef28ff72039a61
PauloSuriani <paulosuriani@gmail.com>
2023-02-20 21:35:06 -0300

    feat: setup newproduct and editproduct components

---
+++ users-data-frontend/src/pages/NewProductForm.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import  { api_url } from '../../serverurl';
+
+type ProductObject = {
+  nome_produto?: string;
+  tipo_produto?: string;
+  valor_sugerido?: string;
+}
+
+export function NewProduct() {
+
+}
+    
