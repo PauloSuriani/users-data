4a6cf9c6712e2e6b6d301fe6ed313f97be61508e
PauloSuriani <paulosuriani@gmail.com>
2023-03-29 21:45:31 -0300

    fix: navigation icons redirect

--- users-data-frontend/src/pages/RoutePage.tsx
+++ users-data-frontend/src/pages/RoutePage.tsx
@@ -138,12 +138,16 @@ export function RoutePage() {
 
         <div className="div-svg-btn-fixed">
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 svg-nav-style" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 svg-nav-style" onClick={() => { navigate('/rotas/nova') }} fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
           </svg>
 
           <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 svg-nav-style" fill="none" onClick={() => { navigate('/') }} viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+          </svg>
+
+          <svg cursor={'pointer'} onClick={() => { navigate('/products') }} style={{ marginRight: '11px' }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="svg-nav-style w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
           </svg>
 
         </div>
